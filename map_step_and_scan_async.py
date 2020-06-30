16450c1be97933ceaa8cb1e53c0ea0626a072b59
ingo_berg <iberg@gmx.info>
2020-06-30 13:35:44 +0200

    imports fixed

--- map_step_and_scan_async.py
+++ map_step_and_scan_async.py
@@ -1,10 +1,9 @@
 import os
 import random
 
-from Mpi.Communication.CommunicatorGpib import CommunicatorGpib
-from Mpi.Devices.Enumerations import GpibCardVendor
-from Mpi.Sentio.ProberSentio import *
-from Mpi.Communication.CommunicatorTcpIp import CommunicatorTcpIp
+from sentio_prober_control.Communication.CommunicatorGpib import CommunicatorGpib
+from sentio_prober_control.Sentio.ProberSentio import *
+from sentio_prober_control.Communication.CommunicatorTcpIp import CommunicatorTcpIp
 
 
 def main():
